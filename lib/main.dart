eb3df5803d853fecd5e897bb2d4c8e5ebff3e04a
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-06 21:22:48 +0100

    Added screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+
 import 'package:firebase_authentication/config/size_config.dart';
-import 'package:firebase_authentication/models/user.dart';
+import 'package:firebase_authentication/constants/widgets/error_widget.dart';
+import 'package:firebase_authentication/constants/widgets/loader.dart';
+import 'package:firebase_authentication/local_storage/user_preference.dart';
+
 import 'package:firebase_authentication/services/firebase_auth_services.dart';
+import 'package:firebase_authentication/views/home.dart';
 import 'package:firebase_authentication/views/login.dart';
+import 'package:firebase_authentication/views/register.dart';
+import 'package:firebase_authentication/views/reset_password.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
@@ -16,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final _init = Firebase.initializeApp();
+    Future<dynamic> userData() => UserPreferences().getUserData();
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
@@ -26,51 +35,44 @@ class MyApp extends StatelessWidget {
     return FutureBuilder(
       future: _init,
       builder: (context, snapshot) {
-        if(snapshot.hasError) {
-          return ErrorWidget();
+       if(snapshot.hasError) {
+          return ErrorScreen();
         } else if (snapshot.hasData) {
           return MultiProvider(
             providers: [
               ChangeNotifierProvider<AuthServices>.value(value: AuthServices()),
-              StreamProvider<AppUser?>.value(value: AuthServices().user, initialData: null)
             ],
             child: MaterialApp(
-              home: Login(),
+              debugShowCheckedModeBanner: false,
+              home: FutureBuilder(
+                  future: Future.wait([userData()]),
+                  builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                     if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Container(
+                        color: Colors.white,
+                      );
+                    }
+                   else if (snapshot.data![0]['email'] != null && snapshot.data![0]['name'] != null) {
+                      return Login();
+                    } else {
+                      return Register();
+                    }
+                  }),
+              routes: {
+                Login.routeName: (ctx) => Login(),
+                Register.routeName: (ctx) => Register(),
+                Home.routeName: (ctx) => Home(),
+                ResetPassword.routeName: (ctx) => ResetPassword(),
+              },
             ),
           );
         } else {
-          Loading();
+          Loader();
         }
         return Container();
         });
       });
-    });
+    }); 
   }
 }
 
-class ErrorWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            Icon(Icons.error),
-            Text('Oops! Something went wrong!')
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Loading extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: CircularProgressIndicator()
-      ),
-    );
-  }
-}
