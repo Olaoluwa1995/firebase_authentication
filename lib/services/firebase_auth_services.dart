7507bb3ebb462a8d3e0c44c6bfa80603e176b225
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-06 21:22:24 +0100

    Updated functionalities

--- lib/services/firebase_auth_services.dart
+++ lib/services/firebase_auth_services.dart
@@ -1,70 +1,82 @@
 import 'dart:io';
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_authentication/models/user.dart';
-import 'package:firebase_authentication/services/database_service.dart';
+import 'package:firebase_authentication/local_storage/user_preference.dart';
 import 'package:flutter/material.dart';
 
 class AuthServices with ChangeNotifier {
-  bool _isLoading = false;
   String _errorMessage = '';
-  bool get isLoading => _isLoading;
   String get errorMessage => _errorMessage;
   FirebaseAuth firebaseAuth = FirebaseAuth.instance;
-
-  void setLoading (val) {
-    _isLoading = val;
-    notifyListeners();
-  }
 
   void setMessage(message) {
     _errorMessage = message;
     notifyListeners();
   }
 
-  AppUser? _userFromFirebaseUser(User? user) {
-    // ignore: unnecessary_null_comparison
-    return user != null ? AppUser(user.uid) : null;
-  }
-
-  Stream<AppUser?> get user {
-    return firebaseAuth.authStateChanges().map(_userFromFirebaseUser);
-  }
-
   Future register (String email, String password, String name) async {
-    setLoading(true);
     try {
-      UserCredential authResult = await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
-      User? user = authResult.user;
-      print(user);
-      
-      await DatabaseService(uid: user!.uid).updateUserData(name);
-      setLoading(false);
-      return _userFromFirebaseUser(user);
+      await firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      UserPreferences().saveUserData(email, name);
+      return true;   
     } on SocketException {
-      setLoading(false);
       setMessage('No internet, please connect to internet');
     } catch (e) {
-      setLoading(false);
-      setMessage(e);
+      return false;
     }
     notifyListeners();
   }
 
   Future login (String email, String password) async {
-    setLoading(true);
     try {
-      UserCredential authResult = await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
-      User? user = authResult.user;
-      setLoading(false);
-      return user;
+      await firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      final userData = await UserPreferences().getUserData();
+      print(userData);
+      return true;   
     } on SocketException {
-      setLoading(false);
       setMessage('No internet, please connect to internet');
     } catch (e) {
-      // print(e.toString());
-      setLoading(false);
       setMessage(e.toString());
+      return false;
+    }
+    notifyListeners();
+  }
+
+  Future sendPasswordResetEmail (String email) async {
+    try {
+      await firebaseAuth.sendPasswordResetEmail(email: email);
+      return true;   
+    } on SocketException {
+      setMessage('No internet, please connect to internet');
+    } catch (e) {
+      setMessage(e.toString());
+      return false;
+    }
+    notifyListeners();
+  }
+
+  // Future verifyCode (String code) async {
+  //   try {
+  //     await firebaseAuth.verifyPasswordResetCode(code);
+  //     return true;   
+  //   } on SocketException {
+  //     setMessage('No internet, please connect to internet');
+  //   } catch (e) {
+  //     setMessage(e.toString());
+  //     return false;
+  //   }
+  //   notifyListeners();
+  // }
+
+  Future confirmPasswordReset (String code, String newPassword) async {
+    try {
+      firebaseAuth.confirmPasswordReset(code: code, newPassword: newPassword);
+      return true;   
+    } on SocketException {
+      setMessage('No internet, please connect to internet');
+    } catch (e) {
+      setMessage(e.toString());
+      return false;
     }
     notifyListeners();
   }
