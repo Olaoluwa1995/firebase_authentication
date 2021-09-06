9513fcfaca7bf73d199069d7af6c7490ebc47a63
Olaoluwa1995 <olakulehinstephen@gmail.com>
2021-09-06 21:23:57 +0100

    Updated files

--- lib/constants/app_fonts.dart
+++ lib/constants/app_fonts.dart
@@ -1,12 +1,19 @@
 import 'package:firebase_authentication/config/size_config.dart';
+import 'package:firebase_authentication/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 // App Fonts Class - Resource class for storing app level font constants
 class AppFonts {
-  static TextStyle valueStyle = TextStyle(
+  static TextStyle heading = TextStyle(
     fontSize: SizeConfig.textMultiplier! * 2.3,
     color: Colors.black,
     fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle link = TextStyle(
+    fontSize: SizeConfig.textMultiplier! * 1.8,
+    fontWeight: FontWeight.bold,
+    color: AppColors.primaryColor,
   );
 
   static TextStyle body1 = TextStyle(
